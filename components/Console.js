245f26a8a7a1da7c4a89bf7fb5d44d61a76253d9
robigan <35210888+robigan@users.noreply.github.com>
2021-07-08 22:05:17 +0200

    console.js actual progress

--- components/Console.js
+++ components/Console.js
@@ -1,22 +1,28 @@
 import styles from "../styles/Console.module.css";
 
-export default function Console({ /** @type {Object} */ConsoleState }) {
+export default function Console() {
+    const ConsoleState = {};
     if (!ConsoleState) throw new Error("ConsoleState is not defined/is falsey");
+    ConsoleState.commands = new Map();
     ConsoleState.lines = [];
+
     ConsoleState.lineAdd = async (Text) => {
         ConsoleState.lines.push(<p className={styles.ConsoleLine}>{Text}</p>);
     };
+    ConsoleState.commands.set("clear", async () => {
+        ConsoleState.lines = [];
+    });
 
-    ConsoleState.lineAdd("Lorem");
-    ConsoleState.lineAdd("Ipsum");
-    ConsoleState.lineAdd("Dolor");
+    const test = async (...test) => {
+        console.log(test);
+    };
 
     return (
         <div className={styles.Console}>
             <div className={styles.ConsoleContent}>
                 {ConsoleState.lines}
             </div>
-            <input className={styles.ConsoleInput} placeholder="Type something here!"></input>
+            <input className={styles.ConsoleInput} placeholder="Type something here!" onInput={test}></input>
         </div>
     );
 }
