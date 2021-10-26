36e50065e1416edc5a8ab3e1f3b63ecdb3fc890e
robigan <35210888+robigan@users.noreply.github.com>
2021-10-26 23:17:58 +0200

    Holy this new rust compiler is QUICK

--- pages/changes.js
+++ pages/changes.js
@@ -21,9 +21,15 @@ export async function getStaticProps() {
 
 function Change({ commit }) {
     return (
-        <>
-            <h1>{commit.commit.message}</h1>
-        </>
+        <div className="container bg-gray-500 bg-opacity-50 rounded m-2">
+            <h1 className="font-bold">Commit Message &quot;{commit.commit.message}&quot;</h1>
+            <h3>ID: {commit.sha}</h3>
+            {
+                commit.commit.verification.verified ? 
+                    <h3 className="text-green-300 border-gray-400 hover:border-green-300 rounded-full border-2 w-min p-1 min-w-max">Verified</h3> : 
+                    <h3 className="text-red-300 border-gray-400 hover:border-red-300 rounded-full border-2 w-min p-1 min-w-max">Not Verified</h3>
+            }
+        </div>
     );
 }
 
