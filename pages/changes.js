cd64d5b7fa2a7648456b2bdb0e66c612949eef7e
robigan <35210888+robigan@users.noreply.github.com>
2021-10-26 22:41:09 +0200

    Loads of shit ngl

--- pages/changes.js
+++ pages/changes.js
@@ -1,5 +1,38 @@
-export default function Changes() {
+import "../styles/changes.module.css";
+
+export async function getStaticProps() {
+    const res = await fetch("https://api.github.com/repos/robigan/robigan-website/commits");
+    const data = await res.json();
+
+    if (!data) {
+        return {
+            props: {
+                commits: []
+            }
+        };
+    }
+
+    return {
+        props: {
+            commits: data
+        },
+    };
+}
+
+function Change({ commit }) {
     return (
-        <h1>This page is still under construction, please comeback later</h1>
+        <>
+            <h1>{commit.commit.message}</h1>
+        </>
+    );
+}
+
+export default function Changes({ commits }) {
+    return (
+        <>
+            {commits.map((commit) => {
+                return Change({ commit: commit});
+            })}
+        </>
     );
 }
