b739b6e83331ef9b72840728de8c1dd8827f9c45
robigan <35210888+robigan@users.noreply.github.com>
2022-08-16 11:36:09 +0200

    This shit hard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
+import profilePicture from "../../public/person.png";
 
 const Home = () => {
     return (
@@ -9,34 +11,55 @@ const Home = () => {
                 <meta name="description" content="Robigan's homepage" />
                 <meta name="theme-color" content="blueviolet" />
             </Head>
+
             <div className="absolute top-0 left-0 w-full h-full">
+
                 <svg viewBox="0 0 1427 355" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M-52 355L1426.31 -74.75H-1530.31L-52 355Z" fill="#FFB400" />
                 </svg>
+
+                {/* The content div absoluted to the top left with full width and height using flex to rearrange items */}
                 <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center lg:flex-row lg:justify-around lg:items-center">
-                    <div className="relative rounded-[50%] min-w-[50%] pt-[50%] border-8 border-gray-800 my-8 lg:w-4/12 lg:h-4/5 lg:rounded-[3rem] lg:border-none lg:my-0 bg-black">
-                        <div className="absolute"></div>
+
+                    {/* Large width screen profile */}
+                    <div className="hidden lg:block relative w-4/12 h-4/5 rounded-[3rem] overflow-hidden bg-black">
+                        <Image src={profilePicture} alt="Robigan's profile picture" layout="fill" objectFit="cover" />
                     </div>
-                    <div className="container my-8 lg:my-0 lg:w-6/12 lg:h-4/5 flex flex-col justify-center">
+
+                    {/* Small width rounded screen profile w/ border */}
+                    <div className="lg:hidden h-64 w-64 relative bg-black rounded-full border-8 border-gray-800 overflow-hidden shrink-0">
+                        <Image src={profilePicture} alt="Robigan's profile picture" layout="fill" objectFit="cover"></Image>
+                    </div>
+
+                    {/* Content area (using flex) */}
+                    <div className="container my-8 lg:my-0 lg:max-w-max lg:w-5/12 lg:h-4/5 flex flex-col justify-center text-center lg:text-left">
+
                         <div className="my-2">
-                            <h1 className="text-[#FFB400]">WELCOME</h1>
+                            <h1 className="text-primary">WELCOME</h1>
                         </div>
+
                         <div className="my-2">
                             <h2>I&apos;M ROBIGAN.</h2>
-                            <p className="text-sm border-t-2 w-fit">A COMPUTER NERD</p>
+                            <p className="text-sm border-t-2 w-fit mx-auto lg:mx-0">A COMPUTER NERD</p>
                         </div>
+
+                        {/* Small about summary */}
                         <div className="my-2">
                             <p>I am an <span className="text-green-500">Ita</span>li<span className="text-red-500">an</span> software engineer.<br />I love creating anything, refining whatever I make down to perfection. Always making sure that what I create is top notch and loved by the community.</p>
                         </div>
+
+                        {/* More about me call to action */}
                         <div className="mb-2 mt-16">
                             <Link href="/about">
-                                <a className="border border-[#FFB400] rounded-full py-4 pl-4">
+                                <a className="border border-primary rounded-full py-4 pl-4">
                                     <span className="mr-4">MORE ABOUT ME</span>
-                                    <span className="rounded-full bg-[#FFB400] p-4"> GO </span>
+                                    <span className="rounded-full bg-primary p-4"> GO </span>
                                 </a>
                             </Link>
                         </div>
+
                     </div>
+
                 </div>
             </div>
         </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: {
-                    dark: "#4f1732",
-                    DEFAULT: "#78244C",
-                    light: "#c43b7d",
-                },
-                background: {
-                    dark: "#202731",
-                    DEFAULT: "#2D4159",
-                },
-            },
-            screens: {
-                "mo-sm": "320px",
-                "mo-md": "411px",
-                "contact": "380px"
+                primary: "#FFB400"
             }
         },
     },
