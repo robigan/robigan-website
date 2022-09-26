7ad6391b6064d1fa844ccfdb189692fb816ca9a1
robigan <35210888+robigan@users.noreply.github.com>
2022-09-26 18:54:52 +0200

    Custom error pages to use same background style

---
+++ src/pages/500.tsx
@@ -0,0 +1,7 @@
+const Custom500 = () => (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h2>500 | Server-side error occured</h2>
+    </div>
+);
+
+export default Custom500;
