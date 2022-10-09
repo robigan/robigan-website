271d5a9768f10738bd0e7360945b1079aa80c815
robigan <35210888+robigan@users.noreply.github.com>
2022-10-09 11:41:53 +0200

    Fix user-select classes on DecoratedButton

--- src/components/FileSystemView/common.tsx
+++ src/components/FileSystemView/common.tsx
@@ -17,40 +17,41 @@ interface DecoratedButtonProps {
  * DecoratedButton is the component used to render the entry for the presence of a node in the File System View hierarchy
  */
 const DecoratedButton:FC<PropsWithChildren<DecoratedButtonProps>> = ({ children, state = true, type }) => {
-    const className = "flex items-center";
+    const className = "flex items-center select-none";
     const size = "1.25rem";
+    const child = <span className="select-text">{children}</span>;
 
     if (type === NodeType.DIR)
         return (
             <h3 className={className}>
                 {state ? <AiOutlineFolderOpen size={size} /> : <AiOutlineFolder size={size} />}
-                &nbsp;{children}
+                &nbsp;{child}
             </h3>
         );
     else if (type === NodeType.FILE)
         return (
             <h3 className={className}>
                 <AiOutlineFile size={size} />
-                &nbsp;{children}
+                &nbsp;{child}
             </h3>
         );
     else if (type === NodeType.ROOT)
         return (
             <h3 className={className}>
                 <AiOutlineRight size={size} />
-                &nbsp;{children}
+                &nbsp;{child}
             </h3>
         );
     else if (type === NodeType.IMAGE)
         return (
             <h3 className={className}>
                 <AiOutlineFileImage size={size} />
-                &nbsp;{children}
+                &nbsp;{child}
             </h3>
         );
     else
         return (
-            <h3 className={className}>{children}</h3>
+            <h3 className={className}>{child}</h3>
         );
 };
 
