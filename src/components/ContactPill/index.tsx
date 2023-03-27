460259ae0a97058041ba3cdd27899d19d9ff0dac
robigan <35210888+robigan@users.noreply.github.com>
2023-03-27 23:35:18 +0200

    Add ContactPill

---
+++ src/components/ContactPill/index.tsx
@@ -0,0 +1,22 @@
+import Link, { LinkProps } from "next/link";
+import { FC, PropsWithChildren } from "react";
+import { BiLinkExternal } from "react-icons/bi";
+
+const BadgeWrapper: FC<PropsWithChildren> = ({ children }) => (
+    <div className="h-full object-contain rounded-full overflow-hidden">
+        {children}
+    </div>
+);
+
+const OpenButton: FC = () => (
+    <BiLinkExternal size="1.25rem" className="text-primary z-10"/>
+);
+
+const ContactPill: FC<PropsWithChildren<LinkProps>> = ({ children, ...linkProps }) => (
+    <Link {...linkProps} className="w-full rounded-full bg-primary border-2 border-primary hover:bg-transparent py-2 px-4 flex flex-row flex-nowrap justify-between items-center text-black-light hover:text-primary transition relative">
+        {children}
+        <OpenButton />
+    </Link>
+);
+
+export default Object.assign(ContactPill, { BadgeWrapper });
