8cc0193e17bc3b5638b7a2df852ca39167d71413
robigan <35210888+robigan@users.noreply.github.com>
2025-05-27 16:25:41 +0200

    Some extra stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxt/image'
-  ]
+  ],
+  css: ['~/assets/css/main.css']
 })
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <UContainer>
+        <h1 class="">I love creating anything, and making it perfection.</h1>
+    </UContainer>
+</template>
